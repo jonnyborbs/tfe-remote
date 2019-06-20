a6d7d502e3051062b33eec3407bd3628896eb466
Jon Schulman <jon@js-tbpro13.local>
2019-06-20 13:29:44 -0700

    new tags

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ data "aws_ami" "ubuntu" {
         instance_type       =       "t2.micro"
 
         tags = {
-            Name = "ImportedFromVCS"
+            Name = "CreatedViaImport"
             ttl = "400"
             owner = "Jon"
         }
