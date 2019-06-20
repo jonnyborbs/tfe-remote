30ceeefd7532e20e1fe9c783f0861c18e3cb7d64
Jon Schulman <jon@js-tbpro13.local>
2019-06-20 11:38:55 -0700

    add main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "jschulman"
+
+    workspaces {
+      name = "imported-vcs-app"
+
+    }
+  }
+}
+provider "aws" {
+    version = "~> 2.0"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+    resource "aws_instance" "web" {
+        ami                 =       "${data.aws_ami.ubuntu.id}"
+        instance_type       =       "t2.micro"
+
+        tags = {
+            Name = "ImportedFromVCS"
+        }
+    }
